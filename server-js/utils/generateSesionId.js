69a5832238b4099031cdacd9de07ee8cd5f7caf2
rakesh-karmaker <rakeshkarmaker0175@gmail.com>
2025-07-02 18:45:46 +0600

    chore: delete the previous js server

--- server-js/utils/generateSesionId.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require("bcrypt");
-const generateId = require("./generateId");
-
-exports.generateSessionId = async () => {
-  const id = generateId();
-  const salt = await bcrypt.genSalt(10); // generate salt of length 10
-  const hash = await bcrypt.hash(id, salt);
-  return hash;
-};
