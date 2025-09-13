e8d77fda86822327afbabded2bb25f779bb1ecaf
rakesh-karmaker <rakeshkarmaker0175@gmail.com>
2025-09-13 19:56:52 +0600

    refactor: remove console logs from OAuth handling in useGoogleOAuth and google routes

--- client/src/hooks/useGoogleOAuth.ts
+++ client/src/hooks/useGoogleOAuth.ts
@@ -33,8 +33,6 @@ export default function useGoogleOAuth(): {
     window.addEventListener("message", async (event) => {
       if (event.origin !== serverURL) return; // important for security
 
-      console.log("Message received from OAuth popup:", event);
-
       const response: OAuthResponse = event.data.user;
       setIsOAuthLoading(false);
 
--- server/src/lib/oAuth/google.ts
+++ server/src/lib/oAuth/google.ts
@@ -35,8 +35,6 @@ router.get("/callback", async (req: Request, res: Response): Promise<void> => {
     grant_type: "authorization_code",
   });
 
-  console.log("Google OAuth token response:", data);
-
   const accessToken = data.access_token as string;
 
   const userInfo = await axios.get(
@@ -45,8 +43,6 @@ router.get("/callback", async (req: Request, res: Response): Promise<void> => {
       headers: { Authorization: `Bearer ${accessToken}` },
     }
   );
-
-  console.log("Google user info:", userInfo.data);
 
   let response: AuthResponse = {
     new: false,
@@ -82,8 +78,6 @@ router.get("/callback", async (req: Request, res: Response): Promise<void> => {
     await redisClient.set(`temp_user:${id}`, JSON.stringify(userInfo.data));
     await redisClient.expire(`temp_user:${id}`, 3600); // 1 hour
   }
-
-  console.log("OAuth response:", response);
 
   res.send(`
     <script>
